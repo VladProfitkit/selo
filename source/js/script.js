56e64de2579fadca3f022fa34172154325fd24a8
VladProfitkit <vlad@profitkit.ru>
2022-10-04 20:36:19 +0300

    Начало верстки шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,54 @@
+let body = $('body');
+
+const openMobileMenu = function() {
+    let mobileMenu = $('.header__bottom');
+
+    mobileMenu.addClass('header__bottom--open');
+    body.addClass('header--fixed');
+}
+
+const closeMobileMenu = function() {
+    let mobileMenu = $('.header__bottom');
+
+    mobileMenu.removeClass('header__bottom--open');
+    body.removeClass('header--fixed');
+}
+
 $(function() {
-    
+    //мобильное меню:
+    let openMenuBtn = $('.header__mobile-btn--open'),
+        closeMenuBtn = $('.header__mobile-btn--close');
+
+    openMenuBtn.on('click', function() {
+        openMobileMenu();
+    });
+
+    closeMenuBtn.on('click', function() {
+        closeMobileMenu();
+    });
+
+    //поиск в шапке:
+    let openSearchBtn = $('.header-search__trap'),
+        closeSearchBtn = $('.header-search__close'),
+        searchScreen = $('.header-search__overlay');
+
+    const openHeaderSearch = function() {
+        searchScreen.addClass('header-search__overlay--open');
+        body.addClass('header--fixed');
+    }
+
+    const closeHeaderSearch = function() {
+        searchScreen.removeClass('header-search__overlay--open');
+        body.removeClass('header--fixed');
+    }
+
+    openSearchBtn.on('click', function() {
+        openHeaderSearch();
+    });
+
+    body.on('mouseup touchend', function(e) {
+        if (searchScreen.is(e.target) || closeSearchBtn.is(e.target)) {
+            closeHeaderSearch();
+        }
+    });
 })
